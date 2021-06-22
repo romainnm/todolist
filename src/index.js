a9183df72e9a0cb77a4ad7cc8d8ef13ea017e871
romainnm <insc.romain@gmail.com>
2021-06-22 09:08:00 -0500

    Added some factory functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import '../dist/css/styles.css'
 
-/* Projects List [All the projects] - Project(name, id)*/
-/* Todolists [all the todolists] - Todolist(name, id)*/
-/* Todolist [list item] - Todolist(task, task description, due date, priority)*/
+/* Storage for all projects / todolists */
+const projects = [];
+const todolists = [];
 
+
+/*[{
+  name: project1,
+  id: p1
+  }, 
+ {
+  name: project2,
+  id: p2
+  }, {
+  name: project3,
+  id: p3
+ }]
+*/
+
+/* Project Factory: construct new projects */
+const project = (name, id) => {
+  return {name, id}
+} 
+
+/* Todolist Factory: construct a new todolist */
+const todolist = (name, id) => {
+  return {name, id}
+}
+
+/* Todolist Task Factory: construct a new todolist task */
+const todolistTask = (name, description, dueDate, priority) => {
+  return {name, description, dueDate, priority}
+}
